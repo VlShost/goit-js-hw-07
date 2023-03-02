11b16ad85b74e74925e85091d46f2ae5629c5cdf
Vladyslav Shostak <113975751+VlShost@users.noreply.github.com>
2023-03-02 21:05:09 +0200

    create markup

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,42 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryRef = document.querySelector('.gallery');
+
+galleryRef.innerHTML = galleryItems
+  .map(({preview, original, description}) => {
+    return `
+    <div class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"
+        />
+      </a>
+    </div>`;
+  })
+  .join('');
+
+  function openModalImg(e) {
+    e.preventDefault();
+
+    if (e.target.nodeName !== "IMG") {
+      return;
+    };
+    
+    function onEscKeyPress(e) {
+        if (e.code === "Escape") {
+            instance.close();
+          }
+    };
+
+    const instance = basicLightbox.create(`<img src="${e.target.dataset.source}" width="1280" height="auto">`, {
+        onShow: () => window.addEventListener("keydown", onEscKeyPress),
+        onClose: () => window.removeEventListener("keydown", onEscKeyPress)
+    });
+    instance.show();
+};
+
+  galleryRef.addEventListener("click", openModalImg);
